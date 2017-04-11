90fa86db9c42a8a5fc673744bad7a9c1bad54136
smeber <sanne@berendschot.nl>
2017-04-11 13:42:23 +0200

    Sanne: Functie cargoLeastFullAircraft
    Functie cargoLeastFullAircraft aangevuld en kleine comments/aanpassingen

--- spacefreight.py
+++ spacefreight.py
@@ -12,15 +12,16 @@ Laurens vd Ziel (10653384)
 import json
 import os
 from operator import itemgetter
+
 def main():
 
 	# Misschien beter om classes te gebruiken in plaats van dicts?
 	# Landen als variabele later en naam scpacecraft als naam dictionary
 	locationList = ['Cygnus', 'Verne ATV', 'Progress', 'Kounotori', 'Ground']
-	USA = {'location': locationList[0],'kg': 2000, 'm3': 18.9}
-	Europe = {'location': locationList[1],'kg': 2300 ,'m3': 13.1};
-	Rusia = {'location': locationList[2],'kg': 2400,'m3': 7.6};
-	Japan = {'location': locationList[3],'kg': 5200,'m3': 14};
+	USA = {'location': locationList[0],'kg': 2000, 'm3': 18.9, 'spaceleft': 18.9}
+	Europe = {'location': locationList[1],'kg': 2300 ,'m3': 13.1, 'spaceleft': 13.1};
+	Russia = {'location': locationList[2],'kg': 2400,'m3': 7.6, 'spaceleft': 7.6};
+	Japan = {'location': locationList[3],'kg': 5200,'m3': 14, 'spaceleft': 14};
 
 	# Import file 
 	# LET OP!! (misschien later nog even netjes 
@@ -36,40 +37,56 @@ def main():
 		# kg/m2 ratio berekenen voor alles in de cargolist
 		cargo['density'] = (cargo['m3']/cargo['kgs'])
 
-	# Sort the cargolist on ???
-	cargoB(cargolist1)
+	# Sort the cargolist on density
+	sortDensity(cargolist1)
 
 # Recursively devide the cargo among the different spacecrafts for part B of the asignment
 def cargoRandom (cargolist1):
 
 	# create random number generator
+	print('TODO')
 
 # Recursively devide the cargo among the different spacecrafts for part B of the asignment
-def cargoB (cargolist):
-
+def sortDensity (cargolist):
+	
 	# Sort bij density http://stackoverflow.com/questions/72899/how-do-i-sort-a-list-of-dictionaries-by-values-of-the-dictionary-in-python
 	sortedCargolist1 = sorted(cargolist, key = lambda k: k['density'], reverse = True)
-
-	print(sortedCargolist1)
+	
+	for cargo in sortedCargolist1:
+		print(cargo)
 
 def cargoLeastFullAircraft (sortedCargolist1):
 
-	spaceLeft = {'Cygnus': 18.9, 'VerneATV': 13.1, 'Progress': 7.6, 'Kounotori': 14}
+	# make array for spaceleft
+	spaceLeft = {USA, Europe, Russia, Japan}
+
+	# check
+	print(spaceLeft)
 
 	# voor elk pakketje in de lijst
 	for cargo in sortedCargolist1:
 
 		# sorteer locatielijst op hoeveel plek er over is
+		spaceLeft = sorted(spaceLeft, key = lambda k: k['spaceleft'], reverse = True)
 
 		# check of er genoeg plek is om het pakketje te plaatsen 
-		if spaceLeft[0] < cargo['kgs']:
+		if spaceLeft[0]['spaceleft'] < cargo['kgs']:
+
+			# check
+			print(spaceLeft)
+			print(cargo)
+
+			# error!! moeten dan gaan ruilen
 			print("Error")
 
 		# zet het pakketje in de minst volle aircraft
-		cargo['location'] = 
+		cargo['location'] = spaceLeft[0]['location']
 
 		# update de locatie over van de aircraft
+		spaceLeft[0]['spaceleft'] -= cargo['kgs']
 
+		# check
+		print(cargo)
 
 if __name__ == '__main__':
 	main()

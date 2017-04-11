f37fa08e6942d5e2501812e1e5c8e4382288a6fe
mrDeLaSoul <lvdziel@gmail.com>
2017-04-11 12:59:24 +0200

    sort array of dict by density
    ll

--- spacefreight.py
+++ spacefreight.py
@@ -12,55 +12,50 @@ Laurens vd Ziel (10653384)
 import json
 import os
 from operator import itemgetter
+def main():
 
-# Misschien beter om classes te gebruiken in plaats van dicts?
-# Landen als variabele later en naam scpacecraft als naam dictionary
-locationList = ['Cygnus', 'Verne ATV', 'Progress', 'Kounotori', 'Ground']
-USA = {'location': locationList[0],'kg': 2000, 'm3': 18.9}
-Europe = {'location': locationList[1],'kg': 2300 ,'m3': 13.1};
-Rusia = {'location': locationList[2],'kg': 2400,'m3': 7.6};
-Japan = {'location': locationList[3],'kg': 5200,'m3': 14};
+	# Misschien beter om classes te gebruiken in plaats van dicts?
+	# Landen als variabele later en naam scpacecraft als naam dictionary
+	locationList = ['Cygnus', 'Verne ATV', 'Progress', 'Kounotori', 'Ground']
+	USA = {'location': locationList[0],'kg': 2000, 'm3': 18.9}
+	Europe = {'location': locationList[1],'kg': 2300 ,'m3': 13.1};
+	Rusia = {'location': locationList[2],'kg': 2400,'m3': 7.6};
+	Japan = {'location': locationList[3],'kg': 5200,'m3': 14};
 
-# Import file 
-# LET OP!! (misschien later nog even netjes 
-# met relative paths werken zodat we de cargolist 
-# bestanden in een losse map kunnen zetten en die vervolgens kunnen accessen = netter
-# Voor nu: zorg ervoor dat Cargolist1 in dezelfde file staat als dit python bestand)
-with open('Cargolist1.json') as cargolistFile1:    
-    cargolist1 = json.load(cargolistFile1)
+	# Import file 
+	# LET OP!! (misschien later nog even netjes 
+	# met relative paths werken zodat we de cargolist 
+	# bestanden in een losse map kunnen zetten en die vervolgens kunnen accessen = netter
+	# Voor nu: zorg ervoor dat Cargolist1 in dezelfde file staat als dit python bestand)
+	with open('Cargolist1.json') as cargolistFile1:    
+	    cargolist1 = json.load(cargolistFile1)
 
-# Create datastructure
-for cargo in cargolist1:
-	cargo['location'] = locationList[4]
-	# kg/m2 ratio berekenen voor alles in de cargolist
-	cargo['density'] = (cargo['m3']/cargo['kgs'])
+	# Create datastructure
+	for cargo in cargolist1:
+		cargo['location'] = locationList[4]
+		# kg/m2 ratio berekenen voor alles in de cargolist
+		cargo['density'] = (cargo['m3']/cargo['kgs'])
 
-# Check hoe cargolist eruit ziet
-print (cargolist1)
+	# Check hoe cargolist eruit ziet
+	
 
-# Sort the cargolist on ???
-sortedCargolist1 = cargolist1.sort(key = itemgetter(4))
+	# Sort the cargolist on ???
+	#sortedCargolist1 = cargolist1.sort(key = itemgetter(4))
+	cargoB(cargolist1)
 
-print(sortedCargolist1)
 # Recursively devide the cargo among the different spacecrafts for part B of the asignment
 def cargoB (cargolist):
 
+	print("hoi")
 	# Sort bij density http://stackoverflow.com/questions/3121979/how-to-sort-list-tuple-of-lists-tuples
-	sortedCargolist1 = sorted(cargolist1, key = lambda tup: tup[1], reverse = True)
+	sortedCargolist1 = sorted(cargolist, key = lambda k: k['density'], reverse = True)
 
-	print
+	print(sortedCargolist1)
 
 
 	# for cargo in sortedCargolist1:
 	# 	if cargo['location'] = locationList[4]:
 
 
-
-
-
-
-
-# # this is the standard boilerplate that calls the main() function
-# if __name__ == '__main__':
-#     # sys.exit(main(sys.argv)) # used to give a better look to exists
-#     main()
+if __name__ == '__main__':
+	main()
